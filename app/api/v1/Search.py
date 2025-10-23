e12c9a8c8825ba405edd1e077f3aef9730f3dd4a
zhadevv <cp.zhadev@gmail.com>
2025-10-23 16:16:09 +0700

    Update Search.py

--- app/api/v1/Search.py
+++ app/api/v1/Search.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Query
 from app.api.Models.Parser import DonghubParser
 from app.api.Models.ApiResponseModels import BaseResponse, SearchResponse
+from app.api.Health import request_counters
 
 router = APIRouter()
 
 @router.get("/search", response_model=BaseResponse)
 async def search_donghua(s: str = Query(..., min_length=1), page: int = Query(1, ge=1)):
+    request_counters["search"] += 1
+    
     async with DonghubParser() as parser:
         data = await parser.scrape_search(s, page)
         
