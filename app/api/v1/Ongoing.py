49bc317bb2afa3bb3998d571aedadbe995e2531f
zhadevv <cp.zhadev@gmail.com>
2025-10-23 16:18:17 +0700

    Update Ongoing.py

--- app/api/v1/Ongoing.py
+++ app/api/v1/Ongoing.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Query
 from app.api.Models.Parser import DonghubParser
 from app.api.Models.ApiResponseModels import BaseResponse, ListResponse
+from app.api.Health import request_counters
 
 router = APIRouter()
 
 @router.get("/ongoing", response_model=BaseResponse)
 async def get_ongoing(page: int = Query(1, ge=1)):
+    request_counters["ongoing"] += 1
+    
     async with DonghubParser() as parser:
         data = await parser.scrape_ongoing(page)
         
