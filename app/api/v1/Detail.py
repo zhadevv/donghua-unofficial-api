e881154842fedbd6737a7eac8ef779f822827c36
zhadevv <cp.zhadev@gmail.com>
2025-10-23 16:19:33 +0700

    Update Detail.py

--- app/api/v1/Detail.py
+++ app/api/v1/Detail.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 from app.api.Models.Parser import DonghubParser
 from app.api.Models.ApiResponseModels import BaseResponse, DetailResponse
+from app.api.Health import request_counters
 
 router = APIRouter()
 
 @router.get("/detail/{slug}", response_model=BaseResponse)
 async def get_detail(slug: str):
+    request_counters["detail"] += 1
+    
     async with DonghubParser() as parser:
         data = await parser.scrape_detail(slug)
         
