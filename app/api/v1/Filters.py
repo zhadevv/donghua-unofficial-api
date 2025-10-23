155f583606007526c21405112bc024f42f8d2924
zhadevv <cp.zhadev@gmail.com>
2025-10-23 16:21:15 +0700

    Update Filters.py

--- app/api/v1/Filters.py
+++ app/api/v1/Filters.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 from app.api.Models.Parser import DonghubParser
 from app.api.Models.ApiResponseModels import BaseResponse
+from app.api.Health import request_counters
 
 router = APIRouter()
 
 @router.get("/filters", response_model=BaseResponse)
 async def get_filters():
+    request_counters["filters"] += 1
+    
     async with DonghubParser() as parser:
         data = await parser.scrape_filters()
         
