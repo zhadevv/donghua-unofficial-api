ba3aee810c13e4ff5cb4cc006b0638692bb600c4
zhadevv <cp.zhadev@gmail.com>
2025-10-23 16:19:13 +0700

    Update Genres.py

--- app/api/v1/Genres.py
+++ app/api/v1/Genres.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Query
 from app.api.Models.Parser import DonghubParser
 from app.api.Models.ApiResponseModels import BaseResponse, GenreResponse
+from app.api.Health import request_counters
 
 router = APIRouter()
 
 @router.get("/genres/{slug}", response_model=BaseResponse)
 async def get_by_genre(slug: str, page: int = Query(1, ge=1)):
+    request_counters["genres"] += 1
+    
     async with DonghubParser() as parser:
         data = await parser.scrape_genres(slug, page)
         
